8e0e2e6d1ddbd917e16e46f31f2ce75b0fa054ec
Gilang Yoenal Marinta <gilym46@gmail.com>
2023-06-14 21:48:43 +0700

    commit

--- app.py
+++ app.py
@@ -1,33 +1,65 @@
-from flask import Flask, request, jsonify
-from PIL import Image
+
 import numpy as np
-import tensorflow as tf
+from PIL import Image
+from io import BytesIO
+from google.cloud import aiplatform
+from google.oauth2 import service_account
+from flask import Flask, request, jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
-@app.route('/resize-image', methods=['POST'])
-def resize_image():
-    # Menerima gambar dari permintaan
+@app.route('/', methods=['GET'])
+def home():
+    return jsonify({'message': 'Sukses'})
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    # Menerima tautan gambar dari payload JSON
     image_file = request.files['image']
     image = Image.open(image_file)
 
-    # Resize gambar
-    resized_image = image.resize((180, 180))
+    
 
     # Mengubah gambar menjadi array NumPy
-    resized_image_data = np.asarray(resized_image)
+    x_test = np.asarray(image)
+    # Perform the resizing operation
+    resized_image_data = image.resize((180, 180))
 
-    # Menambahkan dimensi batch
+    # Add batch dimension
     resized_image_data = np.expand_dims(resized_image_data, axis=0)
 
-    # Konversi tipe data menjadi int16
+    # Convert the data type to int8
     resized_image_data = resized_image_data.astype(np.int16)
 
-    # Mengonversi data menjadi list
+    # Convert the data to a list
     resized_image_data = resized_image_data.tolist()
 
-    # Mengembalikan hasil resize dalam response
-    return jsonify(resized_image_data)
+    # Load credentials from JSON file
+    credentials = service_account.Credentials.from_service_account_file('D:/SEM 6/Capstone/Procecing data/credential.json')
+
+    # Inisialisasi objek endpoint dengan kredensial
+    endpoint = aiplatform.Endpoint(
+        endpoint_name="projects/288463562539/locations/us-central1/endpoints/305220029824106496",
+        credentials=credentials
+    )
+
+    # Make the prediction request
+    prediction = endpoint.predict(instances=resized_image_data).predictions
+
+    # Convert the predicted array to a NumPy array
+    predicted_array = np.array(prediction)
+
+    # Find the index of the highest value
+    highest_class_index = np.argmax(predicted_array)
+    
+
+    result = {
+        'predicted_class': str(highest_class_index)
+    }
+
+    return jsonify(result)
 
 if __name__ == '__main__':
     app.run()
